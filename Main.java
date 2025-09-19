f124c78496c530a2c4f4420e843a1dd22f7edce0
1 <danilsnyiper@gmail.com>
2025-09-19 21:57:02 +0800

    Initial commit

--- Main.java
+++ Main.java
@@ -1,48 +1,128 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
-        System.out.println(dog);
-        System.out.println(cat);
-        System.out.println(paper);
-        var x = dog + 4;
-        var y = cat + 4;
-        var g = paper + 4;
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println(g);
-        var x1 = x - 3.5;
-        var x2 = y - 1.6;
-        var x3 = g - 7639;
-        System.out.println(x1);
-        System.out.println(x2);
-        System.out.println(x3);
+       int x = 7;
+       byte c = 127;
+       short v = 32766;
+       long b = 10000;
+       float n = 3.4f;
+       double m = 1.7;
+       System.out.println(x);
+       System.out.println(c);
+       System.out.println(v);
+       System.out.println(b);
+       System.out.println(n);
+       System.out.println(m);
 
-        var friend = 19;
-        var friend1 = friend + 2;
-        var friend2 = friend1 / 7;
-        System.out.println(friend2);
+       float number = 27.12f;
+       long number2 = 987678965549l;
+       double number3 = 2.786;
+       short number4 = 569;
+       short number5 = -159;
+       short number6 = 27897;
+       byte number7 = 67;
+       System.out.println(number);
+       System.out.println(number2);
+       System.out.println(number3);
+       System.out.println(number4);
+       System.out.println(number5);
+       System.out.println(number6);
+       System.out.println(number7);
 
-        var frog = 3.5;
-        var frog1 = frog * 10;
-        var frog2 = frog1 / 3.5;
-        var frog3 = frog2 + 4;
-        System.out.println(frog3);
+       int lp = 23;
+       int an = 27;
+       int ea = 30;
+       int list = 480;
+       int list1 = lp + an + ea / list;
+       System.out.println("На каждого ученика рассчитано " + list1 + " листов бумаги");
 
-        var oneBox = 78.2;
-        var twoBox = 82.7;
-        System.out.println(oneBox + twoBox);
-        System.out.println(oneBox - twoBox);
-        System.out.println(twoBox % oneBox);
+       int workBootle = 16;
+       int time = 2;
+       int time1 = 20;
+       int minute = 60;
+       int data = 24;
+       int data1 = 3;
+       int mother = 30;
+       int dat = data * data1;
+       int min = time1 / time;
+       int day1 = (minute * data) / time;
+       int dat3 = (dat * minute) / time;
+       int mother1 = data * mother;
+       int moth = (mother1 * minute) / time;
 
-        var hour = 640;
-        var work = 8;
-        var people = hour / work;
-        var mostPeople = people + 94;
-        var work1 = work * mostPeople;
-        System.out.println("Всего работников в компании " + people + " человек");
-        System.out.println("Если в компании работает " + mostPeople + " человек то всего " + work1 + " часов работы может быть поделено между сотрудниками");
+       String time20Minutes = "20 минут";
+       String timeDay = "сутки";
+       String time3Days = "3 дня";
+       String timeMonth = "1 месяц";
+
+       System.out.println("За "+ time20Minutes + " машина произвела " + min + " штук бутылок");
+       System.out.println("За "+ timeDay + " машина произвела " + day1 + " штук бутылок");
+       System.out.println("За " + time3Days + " машина произвела " + dat3 + " штук бутылок");
+       System.out.println("За "+ timeMonth + " машина произвела " + moth + " штук бутылок");
+
+       int jar = 120;
+       int whitePain = 2;
+       int brownPaint = 4;
+       int jar1 = whitePain + brownPaint;
+       int clas = jar / jar1;
+       int white = clas * whitePain;
+       int brown = clas * brownPaint;
+
+       System.out.println("В школе, где " + clas + " классов, нужно " + white + " банок белой краски и " + brown + " банок коричневой краски" );
+
+       int banan = 80;
+       int milk = 105;
+       int plombir = 100;
+       int egg = 70;
+       int banan1 = 5;
+       int milk1 = 2;
+       int plombir1 = 2;
+       int egg1 = 4;
+       int banan2 = banan * banan1;
+       int milk2 = milk * milk1;
+       int plombir2 = plombir * plombir1;
+       int egg2 = egg * egg1;
+       int gr = banan2 + milk2 + plombir2 + egg2;
+       float kg = gr / 1000f;
+       System.out.println("Всего грамм " + gr + " Всего килограмм " + kg);
+
+       int sbros = 7;
+       int sbrosKg = sbros * 1000;
+       int poxyd = 250;
+       int poxyd1 = 500;
+       int sr = (poxyd1 + poxyd) / 2;
+       int tim = sbrosKg / poxyd;
+       int tim2 = sbrosKg / poxyd1;
+       int tim3 = sbrosKg / sr;
+       System.out.println("Если спорстмен трать в день 250 грамм то ему понадобиться " + tim + " дней, если 500 грамм в день то " + tim2 + " дней а в среднем понадобиться " + tim3 + " дней");
+
+       int masha = 67760;
+       int denis = 83690;
+       int kristina = 76230;
+       int god = 12;
+       int mashaGod = masha * god;
+       int denisGod = denis * god;
+       int kristinaGod = kristina * god;
+
+       float proc = 1.1f;
+       float masha1 = masha * proc;
+       float denis1 = denis * proc;
+       float kristina1 = kristina * proc;
+
+       float raznica = (masha1 - masha) * god;
+       float raznica1 = (denis1 - denis) * god;
+       float raznica2 = (kristina1 - kristina) * god;
+
+       System.out.println("Маша теперь получает " + masha1 + " рублей. Годовой доход вырос на " + raznica + " рублей");
+       System.out.println("Маша теперь получает " + denis1 + " рублей. Годовой доход вырос на " + raznica1 + " рублей");
+       System.out.println("Маша теперь получает " + kristina1 + " рублей. Годовой доход вырос на " + raznica2 + " рублей");
+
+
+
+
+
+
+
+
 
 
     }
